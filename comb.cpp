fc8888645410a3c1f9d988c1c297a294332a38fc
Do Ngo <bahuynh619@gmail.com>
2025-08-21 08:59:18 +0700

    Create comb.cpp

---
+++ comb.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+long long comb(int n,int k){
+    if (k==n || k==0) return 1;
+    else return comb(n-1,k-1) + comb(n-1,k);
+}
+int main ()
+{
+    int n,k;
+    cin>>n>>k;
+    cout<<comb(n,k);
+}
