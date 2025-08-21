af2ef3cca2f6c7e9d81b0e8da6bc7dcbe16b99db
Do Ngo <bahuynh619@gmail.com>
2025-08-21 09:11:53 +0700

    Create divisor.cpp

---
+++ divisor.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main ()
+{
+    int n;
+    cin>>n;
+    if (n==0)  cout<<"INF";
+    for(int i=abs(n);i>=1;i--){
+        if (n%i==0){
+            cout<<i<<" ";
+        }
+    }
+}
