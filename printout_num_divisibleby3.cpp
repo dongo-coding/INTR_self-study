a6004bc0fb8d64cd6d7c608dd1dc12b135d8f547
Do Ngo <bahuynh619@gmail.com>
2025-09-14 17:43:01 +0700

    Create printout_num_divisibleby3.cpp

---
+++ printout_num_divisibleby3.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+int a, b; 
+cin >> a >> b;
+b--; a++;
+if ((b-a)<=3){
+    bool ok = false; 
+    for (int i=b; i>=a; i--){
+        if (i%3==0){
+            ok = true; cout << i << " "; 
+        } 
+    }
+    if (not ok) cout << "NOT FOUND"; 
+} 
+else{
+    for (int i=b; i>=a; i--){
+        if (i%3==0) cout << i << " "; 
+    } 
+}
+}
