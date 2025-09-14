4c72ab15e0a15b59b93b01aa240dcc578dd295aa
Do Ngo <bahuynh619@gmail.com>
2025-09-14 17:11:36 +0700

    Create first_equation.cpp

---
+++ first_equation.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main ()
+{
+    int a,b;
+    cin>>a>>b;
+    if(a==0){
+        if(b==0){
+            cout<<"WOW";
+        }
+        else if(b!=0){
+            cout<<"NO";
+        }
+    }
+    if(a!=0){
+        cout<<fixed<<setprecision(2)<<(double)-b/a;
+    }
+}
