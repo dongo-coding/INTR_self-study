86c5bbaa89e8e0c391e00b4bcb7b8e906e28e1ca
Do Ngo <bahuynh619@gmail.com>
2025-09-14 17:20:54 +0700

    Create Simplify_fraction.cpp

---
+++ Simplify_fraction.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+int gcd(int ts,int ms){
+    while(ms!=0){
+        int r=ts%ms;
+        ts=ms;
+        ms=r;
+    }
+    return abs(ts);
+    
+}
+int main ()
+{
+    int ts,ms;
+    cin>>ts>>ms;
+    if(ms < 0){
+        ts = -ts;
+        ms = -ms;
+    }
+    int ucln=gcd(ts,ms);
+    ts/=ucln;
+    ms/=ucln;
+   if(ms==0){
+       cout<<"INVALID";
+   }
+   else if(ms==1){
+       cout<<ts;
+   }
+   else cout<<ts<<" "<<ms;
+}
