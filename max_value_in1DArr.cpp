f4b4d5a8f072a269996c3e2dca8f4366b03560aa
Do Ngo <bahuynh619@gmail.com>
2025-09-14 10:52:42 +0700

    Create max_value_in1DArr.cpp

---
+++ max_value_in1DArr.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main ()
+{
+    int n;
+    cin>>n;
+    vector<int>a(n);
+    for(int i=0;i<n;i++){
+        cin>>a[i];
+    }
+    int max_value=INT_MIN;
+    for(int i=0;i<n;i++){
+        if(a[i]>max_value){
+            max_value=a[i];
+        }
+    }
+    cout<<max_value;
+}
