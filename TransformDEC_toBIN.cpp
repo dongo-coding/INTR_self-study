24c1bec140b429b3ea21e5f1bc58a52129ca50af
Do Ngo <bahuynh619@gmail.com>
2025-09-14 17:22:58 +0700

    Create TransformDEC_toBIN.cpp

---
+++ TransformDEC_toBIN.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+pair<string, int> divideBy2(string num) {
+    string result = "";
+    int carry = 0;
+    for (char c : num) {
+        int current = carry * 10 + (c - '0');
+        result += (current / 2) + '0';
+        carry = current % 2;
+    }
+    
+    result.erase(0, result.find_first_not_of('0'));
+    if (result == "") result = "0";
+    return {result, carry};
+}
+
+string bigDecimalToBinary(string num) {
+    if (num == "0") return "0";
+    string binary = "";
+    while (num != "0") {
+        auto [quotient, remainder] = divideBy2(num);
+        binary = char('0' + remainder) + binary;
+        num = quotient;
+    }
+    return binary;
+}
+
+int main() {
+    int T;
+    cin >> T;
+    vector<string> numbers(T);
+
+    for (int i = 0; i < T; i++) {
+        cin >> numbers[i];
+    }
+    
+    for (int i = 0; i < T; i++) {
+        cout << bigDecimalToBinary(numbers[i]) << endl;
+    }
+}
