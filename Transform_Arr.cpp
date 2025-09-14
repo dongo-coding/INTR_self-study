787bbcaa7254284690e73bb1cd8c49925329b358
Do Ngo <bahuynh619@gmail.com>
2025-09-14 17:15:20 +0700

    Create Transform_Arr.cpp

---
+++ Transform_Arr.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main ()
+{
+    int n;
+    cin>>n;
+    vector<int>a(n);
+    for(int i=0;i<n;i++){
+        cin>>a[i];
+    }
+     for (int i = 1; i < n; i += 2) { 
+        int left = (i - 1 >= 0) ? a[i - 1] : 0;
+        int right = (i + 1 < n) ? a[i + 1] : 0;
+        a[i] += abs(right - left);
+    }
+    for (int i = 0; i < n; i++) {
+        cout << a[i] << " ";
+    }
+}
