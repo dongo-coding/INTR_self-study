a1934246245ea8564cfb1cd8a7b8cf697e969ada
Do Ngo <bahuynh619@gmail.com>
2025-09-14 17:44:32 +0700

    Create Find_largest_difference_InArr.cpp

---
+++ Find_largest_difference_InArr.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    vector<int> a(n);
+
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+
+    int max_val = *max_element(a.begin(), a.end());
+    int min_val = *min_element(a.begin(), a.end());
+
+    cout << max_val - min_val;
+}
