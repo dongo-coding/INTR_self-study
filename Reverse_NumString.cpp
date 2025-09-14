b2457dcb5318f07476aecca9b185a1ff8162a1f3
Do Ngo <bahuynh619@gmail.com>
2025-09-14 17:37:05 +0700

    Create Reverse_NumString.cpp

---
+++ Reverse_NumString.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main () {
+string s;
+
+cin >> s;
+
+while (!s.empty() && s.back() == '0') {
+
+    s.pop_back();
+
+}
+
+reverse(s.begin(), s.end());
+
+cout << s;
+}
