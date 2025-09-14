6fdab48495b244bedfb7655d4fb3c72d37256976
Do Ngo <bahuynh619@gmail.com>
2025-09-14 17:46:06 +0700

    Update Sort_Arr_Advanced.cpp

--- Sort_Arr_Advanced.cpp
+++ Sort_Arr_Advanced.cpp
@@ -13,4 +13,24 @@ sort (a+2, a + n);
 for(int i=1;i<=n;i++){
     cout<<a[i]<<" ";
   }
-}
+} //Way 1
+------------------------------------------------------------------
+#include <bits/stdc++.h>
+using namespace std;
+int main ()
+{
+    int n;
+    cin>>n;
+    vector<int>a(n);
+    for(int i=0;i<n;i++){
+        cin>>a[i];
+    }
+    if(n>2){
+        sort(a.begin()+1,a.end()-1);
+    }
+   for(int i=0;i<n;i++){
+           cout<<a[i]<<" ";
+       }
+
+} //Way 2
+
