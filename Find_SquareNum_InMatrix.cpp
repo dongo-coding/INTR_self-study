8ce42b7ce3fc1b4d2fb2721e6f020fb0c02984c7
Do Ngo <bahuynh619@gmail.com>
2025-10-17 12:39:36 +0700

    Create Find_SquareNum_InMatrix.cpp

---
+++ Find_SquareNum_InMatrix.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+bool isSquareNum(int n){
+    if (n<0) return false;
+    int k=sqrt(n);
+    return k*k==n;
+}
+int main ()
+{
+    int a[100][100];
+    int n,m;
+    cin>>n>>m;
+    for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            cin>>a[i][j];
+        }
+    }
+    
+    vector<int>v;
+    
+    for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            if (isSquareNum(a[i][j])){
+                v.push_back(a[i][j]);
+            }
+        }
+    }
+    
+    if (v.empty()) cout<<"NOT FOUND";
+    
+    else {
+        sort(v.begin(),v.end());
+        
+        v.erase(unique(v.begin(),v.end()),v.end());
+        
+        for(int x : v) cout<<x<<" ";
+    }
+    
+}
