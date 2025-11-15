ea69a76bde63aa7338241b5bb0c92d32fe22c71a
Do Ngo <bahuynh619@gmail.com>
2025-11-15 20:56:21 +0700

    Implement program to separate and print positives and negatives
    This program reads integers, separates them into positive and negative vectors, and prints them.

---
+++ Pos_And_Neg.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    vector<int>a(n);
+    for(int i=0;i<n;i++){
+        cin>>a[i];
+    }
+    vector<int>neg,pos;
+    for(int i=0;i<n;i++){
+        if (a[i]<0) neg.push_back(a[i]);
+        else pos.push_back(a[i]);
+    }
+    for(int i=0;i<neg.size();i++) cout<<neg[i]<<" ";
+    for(int i=0;i<pos.size();i++) cout<<pos[i]<<" ";
+}
