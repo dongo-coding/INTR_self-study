9a7d7bc005a90b65ed548118230cc59da9e1fb01
Do Ngo <bahuynh619@gmail.com>
2025-11-16 09:06:24 +0700

    Implement function to find maximum product of two numbers

---
+++ FindMaxProduct.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+long long findMaxProduct(vector<long long>a){
+    int n=a.size();
+
+    if(n<2) return numeric_limits <long long>::min();
+
+    sort(a.begin(),a.end());
+
+    long long product1 = (long long )a[n-1] * a[n-2];
+    long long product2 = (long long)a[0] * a[1];
+
+    return max(product1,product2);
+
+
+}
+int main()
+{
+    int n;
+    cin>>n;
+    vector<long long>a(n);
+    for(int i=0;i<n;i++){
+        cin>>a[i];
+    }
+    cout<<findMaxProduct(a);
+
+}
